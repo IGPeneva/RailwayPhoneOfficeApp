c1927a34987b794160e9376ca65d946ef845879f
IGPeneva <engr_peneva@abv.bg>
2025-03-13 10:14:36 +0200

    Type of key of user changed to GUID

--- RailwayPhoneOfficeApp.Data/RailwayPhoneOfficeDbContext.cs
+++ RailwayPhoneOfficeApp.Data/RailwayPhoneOfficeDbContext.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using RailwayPhoneOfficeApp.Data.Models;
 
 namespace RailwayPhoneOfficeApp.Data;
 
-public class RailwayPhoneOfficeDbContext : IdentityDbContext
+public class RailwayPhoneOfficeDbContext : IdentityDbContext<ApplicationUser,IdentityRole<Guid>, Guid>
 {
     public RailwayPhoneOfficeDbContext(DbContextOptions<RailwayPhoneOfficeDbContext> options)
         : base(options)
--- RailwayPhoneOfficeApp/Program.cs
+++ RailwayPhoneOfficeApp/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using RailwayPhoneOfficeApp.Data;
+using RailwayPhoneOfficeApp.Data.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,7 +11,7 @@ builder.Services.AddDbContext<RailwayPhoneOfficeDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<RailwayPhoneOfficeDbContext>();
 builder.Services.AddControllersWithViews();
 
